0bd7736532e2455eab263ead32602c4196a680d6
karolsudol <karol@sudoltrust.com>
2023-11-02 17:54:17 +0000

    send mint ixs

--- tokens/scripts/3.createToken.ts
+++ tokens/scripts/3.createToken.ts
@@ -1,4 +1,4 @@
-import { payer, testWallet, connection } from "@/lib/vars";
+import { connection, payer, testWallet } from "@/lib/vars";
 
 import {
     buildTransaction,
@@ -8,8 +8,8 @@ import {
     savePublicKeyToFile,
 } from "@/lib/helpers";
 
-import { Keypair, PublicKey, SystemProgram } from "@solana/web3.js";
 import { MINT_SIZE, TOKEN_PROGRAM_ID, createInitializeMint2Instruction } from "@solana/spl-token";
+import { Keypair, PublicKey, SystemProgram } from "@solana/web3.js";
 
 import {
     PROGRAM_ID as METADATA_PROGRAM_ID,
@@ -54,7 +54,69 @@ import {
 
     console.log("Metadata address:", metadataAccount.toBase58());
 
+    // Create the Metadata account for the Mint
+    const createMetadataInstruction = createCreateMetadataAccountV3Instruction(
+        {
+            metadata: metadataAccount,
+            mint: mintKeypair.publicKey,
+            mintAuthority: payer.publicKey,
+            payer: payer.publicKey,
+            updateAuthority: payer.publicKey,
+        },
+        {
+            createMetadataAccountArgsV3: {
+                data: {
+                    creators: null,
+                    name: tokenConfig.name,
+                    symbol: tokenConfig.symbol,
+                    uri: tokenConfig.uri,
+                    sellerFeeBasisPoints: 0,
+                    collection: null,
+                    uses: null,
+                },
+                // `collectionDetails` - for non-nft type tokens, normally set to `null` to not have a value set
+                collectionDetails: null,
+                // should the metadata be updatable?
+                isMutable: true,
+            },
+        },
+    );
+
+    /**
+     * Build the transaction to send to the blockchain
+     */
+
+    const tx = await buildTransaction({
+        connection,
+        payer: payer.publicKey,
+        signers: [payer, mintKeypair],
+        instructions: [
+            createMintAccountInstruction,
+            initializeMintInstruction,
+            createMetadataInstruction,
+        ],
+    });
+
+    printConsoleSeparator();
+
+    try {
+
+        const sig = await connection.sendTransaction(tx);
 
 
+        console.log("Transaction completed.");
+        console.log(explorerURL({ txSignature: sig }));
+
+        // locally save our addresses for the demo
+        savePublicKeyToFile("tokenMint", mintKeypair.publicKey);
+    } catch (err) {
+        console.error("Failed to send transaction:");
+        console.log(tx);
+
+        const failedSig = await extractSignatureFromFailedTransaction(connection, err);
+        if (failedSig) console.log("Failed signature:", explorerURL({ txSignature: failedSig }));
+
+        throw err;
+    }
 
 })();
