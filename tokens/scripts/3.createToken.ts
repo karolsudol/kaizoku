aae28d4cfc516c3885f60cab7b6b2f66635a6ae6
karolsudol <karol@sudoltrust.com>
2023-11-02 17:51:42 +0000

    metadata

--- tokens/scripts/3.createToken.ts
+++ tokens/scripts/3.createToken.ts
@@ -16,7 +16,45 @@ import {
     createCreateMetadataAccountV3Instruction,
 } from "@metaplex-foundation/mpl-token-metadata";
 
+
 (async () => {
+
+    console.log("Payer address:", payer.publicKey.toBase58());
+    console.log("Test wallet address:", testWallet.publicKey.toBase58());
+
+    const mintKeypair = Keypair.generate();
+    console.log("Mint address:", mintKeypair.publicKey.toBase58());
+
+    const tokenConfig = {
+        decimals: 2,
+        name: "Kaizoku",
+        symbol: "KAI",
+        uri: "https://thisisnot.arealurl/info.json",
+    };
+
+    const createMintAccountInstruction = SystemProgram.createAccount({
+        fromPubkey: payer.publicKey,
+        newAccountPubkey: mintKeypair.publicKey,
+        space: MINT_SIZE,
+        lamports: await connection.getMinimumBalanceForRentExemption(MINT_SIZE),
+        programId: TOKEN_PROGRAM_ID,
+    });
+
+    const initializeMintInstruction = createInitializeMint2Instruction(
+        mintKeypair.publicKey,
+        tokenConfig.decimals,
+        payer.publicKey,
+        payer.publicKey,
+    );
+
+    const metadataAccount = PublicKey.findProgramAddressSync(
+        [Buffer.from("metadata"), METADATA_PROGRAM_ID.toBuffer(), mintKeypair.publicKey.toBuffer()],
+        METADATA_PROGRAM_ID,
+    )[0];
+
+    console.log("Metadata address:", metadataAccount.toBase58());
+
+
 
 
 })();
