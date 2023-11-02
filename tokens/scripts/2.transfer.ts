52b47c918e3d754c574e2613f011e9b10e79b9d4
karolsudol <karol@sudoltrust.com>
2023-11-02 15:45:40 +0000

    new key

--- tokens/scripts/2.transfer.ts
+++ tokens/scripts/2.transfer.ts
@@ -1,6 +1,6 @@
 
-import { payer, testWallet, connection, STATIC_PUBLICKEY } from "@/lib/vars";
 import { explorerURL, printConsoleSeparator } from "@/lib/helpers";
+import { STATIC_PUBLICKEY, connection, payer, testWallet } from "@/lib/vars";
 
 import { SystemProgram, TransactionMessage, VersionedTransaction } from "@solana/web3.js";
 
@@ -12,7 +12,7 @@ import { SystemProgram, TransactionMessage, VersionedTransaction } from "@solana
     const space = 0;
     const balanceForRentExemption = await connection.getMinimumBalanceForRentExemption(space);
 
-    const createAccountIx = SystemProgram.createAccount({
+    const createTestAccountIx = SystemProgram.createAccount({
         fromPubkey: payer.publicKey,
         newAccountPubkey: testWallet.publicKey,
         lamports: balanceForRentExemption + 2_000_000,
@@ -27,8 +27,33 @@ import { SystemProgram, TransactionMessage, VersionedTransaction } from "@solana
         programId: SystemProgram.programId,
     });
 
+    const transferToStaticWalletIx = SystemProgram.transfer({
+        lamports: 100_000,
+        fromPubkey: payer.publicKey,
+        toPubkey: STATIC_PUBLICKEY,
+        programId: SystemProgram.programId,
+    });
+    let recentBlockhash = await connection.getLatestBlockhash().then(res => res.blockhash);
 
+    const message = new TransactionMessage({
+        payerKey: payer.publicKey,
+        recentBlockhash,
+        instructions: [
+            createTestAccountIx,
+            transferToStaticWalletIx,
+            transferToTestWalletIx,
+            transferToStaticWalletIx,
+        ],
+    }).compileToV0Message();
 
+    const tx = new VersionedTransaction(message);
+    tx.sign([payer, testWallet]);
+
+    const sig = await connection.sendTransaction(tx);
+    printConsoleSeparator();
+
+    console.log("Transaction completed.");
+    console.log(explorerURL({ txSignature: sig }));
 
 
 })();
