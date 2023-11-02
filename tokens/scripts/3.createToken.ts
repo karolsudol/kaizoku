fdc800a27c48ffa7c694f1342348335d40eae398
karolsudol <karol@sudoltrust.com>
2023-11-02 16:07:25 +0000

    imports

--- tokens/scripts/3.createToken.ts
+++ tokens/scripts/3.createToken.ts
@@ -1,1 +1,22 @@
+import { payer, testWallet, connection } from "@/lib/vars";
 
+import {
+    buildTransaction,
+    explorerURL,
+    extractSignatureFromFailedTransaction,
+    printConsoleSeparator,
+    savePublicKeyToFile,
+} from "@/lib/helpers";
+
+import { Keypair, PublicKey, SystemProgram } from "@solana/web3.js";
+import { MINT_SIZE, TOKEN_PROGRAM_ID, createInitializeMint2Instruction } from "@solana/spl-token";
+
+import {
+    PROGRAM_ID as METADATA_PROGRAM_ID,
+    createCreateMetadataAccountV3Instruction,
+} from "@metaplex-foundation/mpl-token-metadata";
+
+(async () => {
+
+
+})();
