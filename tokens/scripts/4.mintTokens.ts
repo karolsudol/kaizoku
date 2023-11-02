51cdeeb6a4305dfa79c569285e5a89c6eda444e5
karolsudol <karol@sudoltrust.com>
2023-11-02 18:09:16 +0000

    mint sucess

---
+++ tokens/scripts/4.mintTokens.ts
@@ -0,0 +1,47 @@
+
+import { payer, connection } from "@/lib/vars";
+import { explorerURL, loadPublicKeysFromFile } from "@/lib/helpers";
+
+import { PublicKey } from "@solana/web3.js";
+import { getOrCreateAssociatedTokenAccount, mintTo } from "@solana/spl-token";
+
+(async () => {
+
+    console.log("Payer address:", payer.publicKey.toBase58());
+
+    let localKeys = loadPublicKeysFromFile();
+
+    if (!localKeys?.tokenMint)
+        return console.warn("No local keys were found. Run '3.createToken.ts'");
+
+    const tokenMint: PublicKey = localKeys.tokenMint;
+
+    console.log("==== Local PublicKeys loaded ====");
+    console.log("Token's mint address:", tokenMint.toBase58());
+    console.log(explorerURL({ address: tokenMint.toBase58() }));
+
+
+    const tokenAccount = await getOrCreateAssociatedTokenAccount(
+        connection,
+        payer,
+        tokenMint,
+        payer.publicKey,
+    ).then(ata => ata.address);
+
+    console.log("Token account address:", tokenAccount.toBase58());
+
+    const amountOfTokensToMint = 1_000;
+
+    // mint some token to the "ata"
+    console.log("Minting some tokens to the ata...");
+    const mintSig = await mintTo(
+        connection,
+        payer,
+        tokenMint,
+        tokenAccount,
+        payer,
+        amountOfTokensToMint,
+    );
+
+    console.log(explorerURL({ txSignature: mintSig }));
+})();
