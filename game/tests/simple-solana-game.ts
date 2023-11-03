780c85622d3fae5dea20c34c532d48c9fd57c7d7
karolsudol <karol@sudoltrust.com>
2023-11-03 16:21:05 +0000

    test

--- game/tests/simple-solana-game.ts
+++ game/tests/simple-solana-game.ts
@@ -1,16 +1,89 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
-import { SimpleSolanaGame } from "../target/types/simple_solana_game";
+import { assert } from "chai";
+import { TinyAdventure } from "../target/types/tiny_adventure";
 
-describe("simple-solana-game", () => {
+describe("tiny-adventure", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  anchor.setProvider(anchor.AnchorProvider.env())
 
-  const program = anchor.workspace.SimpleSolanaGame as Program<SimpleSolanaGame>;
+  const program = anchor.workspace.TinyAdventure as Program<TinyAdventure>
+  const wallet = anchor.workspace.TinyAdventure.provider.wallet
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
-});
+  // PDA for the game data account
+  const [newGameDataAccount] = anchor.web3.PublicKey.findProgramAddressSync(
+    [Buffer.from("level1", "utf8")],
+    program.programId
+  )
+
+  it("Initialize", async () => {
+    // Initialize the game data account
+    const tx = await program.methods
+      .initialize()
+      .accounts({
+        newGameDataAccount: newGameDataAccount,
+        signer: wallet.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .rpc()
+
+    // Fetch the game data account
+    const gameDataAccount = await program.account.gameDataAccount.fetch(
+      newGameDataAccount
+    )
+    assert(gameDataAccount.playerPosition == 0)
+
+    console.log(
+      "Player position is:",
+      gameDataAccount.playerPosition.toString()
+    )
+  })
+
+  it("Run Right", async () => {
+    // Move right 3 times
+    for (let i = 0; i < 3; i++) {
+      const tx = await program.methods
+        .moveRight()
+        .accounts({
+          gameDataAccount: newGameDataAccount,
+        })
+        .rpc()
+    }
+
+    // Fetch the game data account
+    const gameDataAccount = await program.account.gameDataAccount.fetch(
+      newGameDataAccount
+    )
+
+    assert(gameDataAccount.playerPosition == 3)
+
+    console.log(
+      "Player position is:",
+      gameDataAccount.playerPosition.toString()
+    )
+  })
+
+  it("Run Left", async () => {
+    // Move left 3 times
+    for (let i = 0; i < 3; i++) {
+      const tx = await program.methods
+        .moveLeft()
+        .accounts({
+          gameDataAccount: newGameDataAccount,
+        })
+        .rpc()
+    }
+
+    // Fetch the game data account
+    const gameDataAccount = await program.account.gameDataAccount.fetch(
+      newGameDataAccount
+    )
+
+    assert(gameDataAccount.playerPosition == 0)
+
+    console.log(
+      "Player position is:",
+      gameDataAccount.playerPosition.toString()
+    )
+  })
+})
