6b24384f8f95c0cb2acd69de266a949f1a626e84
karolsudol <karol@sudoltrust.com>
2023-12-10 12:31:37 +0000

    img page

--- sol1/src/routes/index.tsx
+++ sol1/src/routes/index.tsx
@@ -1,17 +1,18 @@
 import Counter from "~/components/Counter";
 import "./index.css";
+import pirateImage from "~/components/pirate.jpeg"; // Ensure the path is correct
 
 export default function Home() {
   return (
     <main>
-      <h1>Hello world!</h1>
+      <h1>kaizkoku!</h1>
       <Counter />
       <p>
         Visit{" "}
-        <a href="https://solidjs.com" target="_blank">
-          solidjs.com
+        <a href="https://github.com/karolsudol/kaizoku" target="_blank">
+          <img src={pirateImage} alt="Kaizoku" class="responsive-image" />
         </a>{" "}
-        to learn how to build Solid apps.
+        to learn how to build Solana - Solid apps.
       </p>
     </main>
   );
